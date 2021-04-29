3dfc35718e62aca4f553d833aa3e8795547d4bb5
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2021-04-29 12:08:42 +0530

    updated frontend for all models

--- DeployModel/views.py
+++ DeployModel/views.py
@@ -11,44 +11,54 @@ def home(request):
     return render(request, "home.html")
 
 def result(request):
-    # return render(request, 'result.html')
-    model = keras.models.load_model("D:\VSCode\Stock-Price-LSTM\ML_models\apple.h5")
+    # load all the models
+    apple_model = keras.models.load_model("D:\VSCode\Stock-Price-LSTM\ML_models\first.h5")
+    google_model = keras.models.load_model("D:\VSCode\Stock-Price-LSTM\ML_models\goog.h5")
+    microsoft_model = keras.models.load_model("D:\VSCode\Stock-Price-LSTM\ML_models\msft.h5")
+    
+    # for scaling the testing dataset
     scaler = MinMaxScaler(feature_range=(0, 1))
 
-    startDate = request.GET['start_date']
-    endDate = request.GET['end_date']
-    data = web.DataReader('AAPL', data_source='yahoo', start= startDate, end= endDate)
-    # new
+    # get data for testing
+    company_name = request.GET['company-name']
+    date = request.GET['date']
 
+    # get respective data of company
+    if(company_name == 'apple'):
+        data = web.DataReader('AAPL', data_source='yahoo', start= '26-04-2020', end= date)
+    elif(company_name == 'microsoft'):
+        data = web.DataReader('MSFT', data_source='yahoo', start= '26-04-2020', end= date)
+    else:
+        data = web.DataReader('GOOG', data_source='yahoo', start= '26-04-2020', end= date)
+    # filter the data to get closing price ot of whole dataset
     data_filtered = data.filter(['Close'])
+    
     # form data for scaling
     dataset = data_filtered.values
     scaler.fit_transform(dataset)
+    
     # take out last 60 days dataset for prediction
     last_60_days = data_filtered[-60:].values
     last_60_days_scaled = scaler.transform(last_60_days)
+    
     # form a test dataset
     X_test = []
     X_test.append(last_60_days_scaled)
     X_test = np.array(X_test)
+    
     # reshape dataset for deep learning LSTM model
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    # get prediction
-    pred_price = model.predict(X_test)
+    
+    # get prediction with respective model
+    if(company_name == 'apple'):
+        pred_price = apple_model.predict(X_test)
+    elif(company_name == 'microsoft'):
+        pred_price = microsoft_model.predict(X_test)
+    else:
+        pred_price = google_model.predict(X_test)
+    # inverse the scaling
     pred_price = scaler.inverse_transform(pred_price)
-    # print("Predicted price for next day: ", pred_price)
-
-
-    # print(new)
-    # pred = np.array(new.set_index("Close"))
-    # p = pred[0][4]
-    # p = np.array(p).reshape(1,1,1)
-    # p
-
-
     
-    # ans = scaler.predict(p)
+    # return predicted price to frontend
     ans = pred_price.reshape(1)[0]
-    # print(ans)
-    
     return render(request, "result.html", {'ans': ans})
