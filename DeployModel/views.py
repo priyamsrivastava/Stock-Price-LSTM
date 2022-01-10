97e15f22940fd2894fe74bb9df70086b2d6c790c
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2022-01-10 18:16:31 +0530

    updated

--- DeployModel/views.py
+++ DeployModel/views.py
@@ -1,5 +1,5 @@
-import joblib
 import numpy as np
+import pandas as pd
 import keras
 import pandas_datareader as web
 from django.http import HttpResponse
@@ -10,28 +10,35 @@ from sklearn.preprocessing import MinMaxScaler
 def index(request):
     return render(request, "index.html")
 
+def invTransform(scaler, data, colName, colNames):
+    dummy = pd.DataFrame(np.zeros((len(data), len(colNames))), columns=colNames)
+    dummy[colName] = data
+    dummy = pd.DataFrame(scaler.inverse_transform(dummy), columns=colNames)
+    return dummy[colName].values
+
 def result(request):
     # load all the models
-    apple_model = keras.models.load_model(".\pple.h5")
-    google_model = keras.models.load_model(".\goog.h5")
-    microsoft_model = keras.models.load_model(".\msft.h5")
+    infy_model = keras.models.load_model(".\infy.h5")
+    ttm_model = keras.models.load_model(".\motors.h5")
+    acc_model = keras.models.load_model(".\cem.h5")
     
     # for scaling the testing dataset
     scaler = MinMaxScaler(feature_range=(0, 1))
 
-    # get data for testing
+    # fetch required data
     company_name = request.GET['company-name']
     date = request.GET['date']
 
     # get respective data of company
-    if(company_name == 'apple'):
-        data = web.DataReader('AAPL', data_source='yahoo', start= '26-04-2020', end= date)
-    elif(company_name == 'microsoft'):
-        data = web.DataReader('MSFT', data_source='yahoo', start= '26-04-2020', end= date)
+    if(company_name == 'Infosys'):
+        data = web.DataReader('INFY', data_source='yahoo', start= '01-10-2021', end= date)
+    elif(company_name == 'Tata Motors'):
+        data = web.DataReader('TTM', data_source='yahoo', start= '01-10-2021', end= date)
     else:
-        data = web.DataReader('GOOG', data_source='yahoo', start= '26-04-2020', end= date)
+        data = web.DataReader('ACC', data_source='yahoo', start= '01-10-2021', end= date)
+    
     # filter the data to get closing price ot of whole dataset
-    data_filtered = data.filter(['Close'])
+    data_filtered = data.filter(items = ['High', 'Low', 'Open', 'Close', 'Volume'])
     
     # form data for scaling
     dataset = data_filtered.values
@@ -46,18 +53,16 @@ def result(request):
     X_test.append(last_60_days_scaled)
     X_test = np.array(X_test)
     
-    # reshape dataset for deep learning LSTM model
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    
     # get prediction with respective model
-    if(company_name == 'apple'):
-        pred_price = apple_model.predict(X_test)
-    elif(company_name == 'microsoft'):
-        pred_price = microsoft_model.predict(X_test)
+    if(company_name == 'Infosys'):
+        pred_price = infy_model.predict(X_test)
+    elif(company_name == 'Tata Motors'):
+        pred_price = ttm_model.predict(X_test)
     else:
-        pred_price = google_model.predict(X_test)
+        pred_price = acc_model.predict(X_test)
+    
     # inverse the scaling
-    pred_price = scaler.inverse_transform(pred_price)
+    pred_price = invTransform(scaler, pred_price, 'Close', ['High', 'Low', 'Open', 'Close', 'Volume'])
     
     # return predicted price to frontend
     ans = pred_price.reshape(1)[0]
